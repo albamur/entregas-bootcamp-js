0e10719d8b8af8658571a1d19571cb332ec6ed7a
albamur <albamrpz@gmail.com>
2024-03-13 20:21:53 +0100

    Modulo 5 - Reorganización y separación en funciones

--- 05-condicionales/src/main.ts
+++ 05-condicionales/src/main.ts
@@ -20,242 +20,199 @@ Para jugar al juego de las siete y media en solitario, sigue estos pasos:
 
 Es importante recordar que las cartas numéricas valen su valor nominal, es decir, el As (uno de cada palo) vale 1 punto, las cartas del 2 al 7 valen su valor nominal y las figuras (sota, caballo y rey) valen medio punto cada una.
  */
+let contadorJugador = 0
 
-
-// función para obtener carta
-
-const btnInicio = document.getElementById("inicio")
-const txtCogeCarta = document.getElementById("txtCogeCarta")
-
-function inicio(){
-    
-    btnInicio?.removeEventListener('click', inicio)
-    btnCogerCarta?.addEventListener('click', pideCarta)
-
-    if(btnInicio) {
-        btnInicio.style.display = 'none'
-    }
-
-    if(txtCogeCarta){
-        txtCogeCarta.innerHTML = 'Coge una carta'
-    }
-
-    if(prfResultado){
-        prfResultado.innerHTML = ''
-    }
-}
-document.addEventListener("DOMContentLoaded", inicio);
-
-
-/**1. Mostrar puntuación
-Arranca por crear una variable que almacena la puntuación que lleve el usuario:
-
-    Crea una variable para almacenar la puntuación, inicialmente será 0. ✅
-    Crea un div en el HTML en el que podamos mostrar la puntuación. ✅ <p id="numero"></p>
-    Crea una función que se llame muestraPuntuacion que muestre la puntuación actual en el div. ✅
-    Invoca a la función en cuanto este disponible el DOM. ✅
-Más adelante invocaremos muestraPuntuación cada vez que el usuario pida carta nueva. */
-let barajaJugador = 0;
-
-// función para mostrar puntuación
-function mostrarPuntuacion(){
-    if (prfNumJugador) {
-        prfNumJugador.innerHTML = "Puntuación jugador: " + barajaJugador + ""
-    }
-}
-document.addEventListener("DOMContentLoaded", mostrarPuntuacion);
-
-
-/** 2. Pedir carta
-Implementa la funcionalidad de pedir carta, ¿En qué consiste?
-    - Hay que generar una función que nos devuelva una carta aleatoria, la podemos llamar dameCarta.✅
-    - Para ello exponemos un botón en el HTML que al pulsarlo llame a la función dameCarta. ✅
-    - Para probar este caso, de momento muestra la carta elegida por consola.
-*/
 const divBarajaJugador = document.getElementById("barajaJugador")
+document.addEventListener("DOMContentLoaded", comenzarPartida);
 
-function dameCarta(){
-    //numero random del 1 al 10
-    let randomNum =  Math.floor(Math.random()*(11-1)+1);
-   
-    if (randomNum > 7){
-        return randomNum+=2
-    } else {
-        return randomNum
-    }
+const btnReset = document.getElementById("reset")
+if (btnReset && btnReset instanceof HTMLButtonElement) {
+    btnReset.addEventListener("click", comenzarPartida);
 }
 
-/**3. Mostrar carta
-Crea una función que se llame muestraCarta que muestre la carta que le pasemos por parámetro ✅ 
-    - Añade un img en el HTML en el que podamos mostrar la carta. ✅
-    - Ese img va a tener un src que va a ser la url de la imagen de la carta ✅
-    - Crea una función mostrar carta, para mapear valor a imagen de carta puedes utilizar un switch para hacer la conversión. ✅
-    - Cuando el usuario pulse en el bóton Pide Carta llama a pideCarta y con el resultado llama a mostrarCarta. ✅
-*/
+function comenzarPartida(){
 
-function muestraCarta(carta: number) :void{
-    let urlCarta = ''
+    contadorJugador = 0
+
+    if(divBarajaJugador && divBarajaJugador instanceof HTMLDivElement){
+        divBarajaJugador.innerHTML = ''
+    }
+
+    pintarResultado('')
+    
+    btnCogerCartaActivado(true)
+    btnInicioActivado(false)
+}
+
+
+
+const btnCogerCarta = document.getElementById("cogerCarta")
+if (btnCogerCarta && btnCogerCarta instanceof HTMLButtonElement) {
+    btnCogerCarta.addEventListener("click", pedirCarta);
+}
+
+function pedirCarta() {
+    //Crear número aleatorio
+    const numeroAleatorio = crearNumeroAleatorio()
+    //Transformar el número a carta
+    const carta = transformarNumeroAleatorioACarta(numeroAleatorio)
+    //Transformar carta a url
+    const url = transformarCartaAUrl(carta)
+    //Añadir url a src de la img
+    pintarUrlDeLaCarta(url)
+    //Sumar valor carta al contador
+    contadorJugador = sumarValorCartaAContador(carta)
+    //Añadir contador al prf
+    pintarContador(contadorJugador)
+    //CComprobar puntuación partida
+    const resultado = comprobarPartida()
+    //Pintar resultado
+    pintarResultado(resultado)
+    //Reactivar el boton de inicio
+    btnInicioActivado(true)
+}
+
+
+const btnPlantarse = document.getElementById("plantarse")
+if (btnPlantarse && btnPlantarse instanceof HTMLButtonElement) {
+    btnPlantarse.addEventListener("click", plantarsePartida);
+}
+
+function plantarsePartida() {
+    btnCogerCartaActivado(false)
+    //Comprobar puntuación
+    let msg=''
+    if(contadorJugador < 4) {
+        msg = "Has sido muy conservador"
+    } else if (contadorJugador >= 4 && contadorJugador <= 5) {
+        msg = "Te ha entrado el canguelo eh?"
+    } else if (contadorJugador > 5 && contadorJugador <= 7){
+        msg = "Casi casi..."
+    } 
+
+    //Pintar el resultado
+    pintarResultado(msg)
+    btnInicioActivado(true)
+}
+
+
+
+///-----------------------------
+
+function crearNumeroAleatorio(): number {
+    return Math.floor(Math.random()*(11-1)+1);
+}
+
+function transformarNumeroAleatorioACarta(numeroAleatorio: number) :number {
+    if (numeroAleatorio > 7) {
+        return numeroAleatorio + 2
+    }
+
+    return numeroAleatorio
+}
+
+function transformarCartaAUrl(carta: number) : string{
     switch (carta) {
         case 1:
-            urlCarta = '1_as'
-            break;
+            return 'https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/1_as-copas.jpg'
         case 2:
-            urlCarta = '2_dos'
-            break;
+            return 'https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/2_dos-copas.jpg'
         case 3:
-            urlCarta = '3_tres'
-            break;
+            return 'https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/3_tres-copas.jpg'
         case 4:
-            urlCarta = '4_cuatro'
-            break;
+            return 'https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/4_cuatro-copas.jpg'
         case 5:
-            urlCarta = '5_cinco'
-            break;
+            return 'https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/5_cinco-copas.jpg'
         case 6:
-            urlCarta = '6_seis'
-            break;
+            return 'https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/6_seis-copas.jpg'
         case 7:
-            urlCarta = '7_siete'
-            break;
+            return 'https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/7_siete-copas.jpg'
         case 10:
-            urlCarta = '10_sota'
-            break;
+            return 'https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/10_sota-copas.jpg'
         case 11:
-            urlCarta = '11_caballo'
-            break;
+            return 'https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/11_caballo-copas.jpg'
         case 12:
-            urlCarta = '12_rey'
-        break;
-
+            return 'https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/12_rey-copas.jpg'
         default:
-            break;
+            return `https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/back.jpg`
     }
+}
 
-    if(divBarajaJugador){
-        let imgCarta = document.createElement("img")
-        imgCarta.setAttribute('src', `https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/${urlCarta}-copas.jpg`)
-        imgCarta.setAttribute('class','cartaJugador')
-        
+function pintarUrlDeLaCarta(url: string) : void{
+    
+    if(divBarajaJugador && divBarajaJugador instanceof HTMLDivElement){
+        const imgCarta = document.createElement("img")
+
+        if(imgCarta && imgCarta instanceof HTMLImageElement){
+            imgCarta.src = url
+            imgCarta.className = 'cartaJugador'
+        }
         divBarajaJugador.appendChild(imgCarta)
     }
 }
 
-function pideCarta(){
-    let carta = dameCarta()
-    muestraCarta(carta)
-    actualizarPuntuacion(carta)    
-}
-
-/**3. Sumar puntuación
-Una vez que le hemos dado la carta al usuario, tenemos que sumar la puntuación de la carta a la puntuación total.
-
-Pistas
-    - Tenemos un div donde mostramos la puntuación y tenemos una variable donde la almacenamos. ✅
-    - Suma el nuevo valor y llama a la función que creamos previamente para mostrar la información. ✅
- */
-const prfNumJugador = document.getElementById("numero")
-
-function actualizarPuntuacion (carta: number) {
-    console.log(`Actualizar puntuacion ${carta}`);
-    
-    //actualizar puntuacion
-    if (carta > 7) { barajaJugador = barajaJugador + 0.5 } 
-    else { barajaJugador = barajaJugador + carta }
-
-    if (barajaJugador > 7.5){ gameOver() }
-    
-    mostrarPuntuacion()
-}
-
-/** 4. Game over
-Si el usuario se pasa de 7,5 puntos, el juego termina y se muestra un mensaje de Game Over, ✅
-a demás el usuario no puede seguir pidiendo cartas. ✅
-*/
-const btnCogerCarta = document.getElementById("cogerCarta")
-
-function gameOver() {
-    if(prfResultado){
-        prfResultado.innerHTML = "Game Over"
+function sumarValorCartaAContador(carta:number) {
+    if (carta > 7) {
+        return contadorJugador + 0.5
     }
-
-    btnCogerCarta?.removeEventListener('click', pideCarta)
+    return contadorJugador + carta
 }
 
-
-/**5. Me planto
-Añadir un botón para que el usuario pueda plantarse, si el usuario se planta, el juego termina, el usuario no puede pedir más cartas y:
-    - Si su puntuación es menor que 4, mostrar un mensaje que diga "Has sido muy conservador".
-    - Si la puntuación ha sido 5, mostrar un mensaje que diga "Te ha entrado el canguelo eh?".
-    - Si la puntuación ha sido 6 o 7, mostrar un mensaje que diga... "Casi casi...".
-    - Si la puntuación es 7.5, mostrar un mensaje que diga "¡ Lo has clavado! ¡Enhorabuena!" */
-
-
-// función para obtener carta
-
-const prfResultado = document.getElementById("resultado")
-const btnPlantarse = document.getElementById("plantarse")
-btnPlantarse?.addEventListener('click', plantarse)
-
-type Resultado = 
-    | "Has sido muy conservador" 
-    | "Te ha entrado el canguelo eh?"
-    | "Casi"
-    | "¡Lo has clavado! ¡Enhorabuena!"
-    | "Número de carta indefinido";
-
-function plantarse(){
-    gameOver()
-
-    if(prfResultado) {
-        prfResultado.innerHTML = mensajeResultado()
+function pintarContador(contadorActualizado:number) : void{
+    const prfNumJugador = document.getElementById("numero")
+    if (prfNumJugador && prfNumJugador instanceof HTMLParagraphElement) {
+        prfNumJugador.innerHTML = "Puntuación jugador: " + contadorActualizado + ""
     }
 }
 
-function mensajeResultado() : Resultado{
-    if(barajaJugador < 4){
-        return "Has sido muy conservador"
-    } else if(barajaJugador ==  5){
-        return "Te ha entrado el canguelo eh?"
-    } else if(barajaJugador ==  6 || barajaJugador ==  7){
-        return "Casi"
-    } else if(barajaJugador ==  7.5){
+function comprobarPartida() :string {
+    if (contadorJugador > 7.5) {
+        btnCogerCartaActivado(false)
+        btnInicioActivado(true)
+        return "Game Over"
+    }
+
+    if(contadorJugador === 7.5) {
+        btnCogerCartaActivado(false)
+        btnInicioActivado(true)
         return "¡Lo has clavado! ¡Enhorabuena!"
-    } else {
-        return "Número de carta indefinido"
+    }
+
+    return '¿Quieres plantarte con lo que tienes?'
+}
+
+function pintarResultado(mensaje: string) : void{
+    const prfResultado = document.getElementById("resultado")
+    console.log(mensaje);
+    
+    if(prfResultado && prfResultado instanceof HTMLHeadingElement){
+        prfResultado.innerHTML = mensaje
+        console.log(prfResultado.innerHTML);
     }
 }
 
-
-/**6. Nueva partida
-Una vez que el usuario ha terminado la partida (sea porque se ha plantado o porque ha perdido), 
-se le muestra un botón para que pueda empezar una nueva partida. 
-*/
-
-function reset(){
-    barajaJugador=0
-
-    if(prfNumJugador){
-        prfNumJugador.innerHTML = ""
-    }
-    mostrarPuntuacion()
-
-    if(divBarajaJugador){
-       divBarajaJugador.innerHTML="" 
-    } 
-
-    if(txtCogeCarta){
-        txtCogeCarta.innerHTML = ''
-    }
-    
-
-    btnInicio?.addEventListener('click', inicio)
-    btnCogerCarta?.removeEventListener('click', pideCarta)
-    
-    if(btnInicio) {
-        btnInicio.style.display = 'inline'
-    }
+function btnInicioActivado(activado: boolean) {
+    const btnInicio = document.getElementById("inicio")
+    if (btnInicio && btnInicio instanceof HTMLButtonElement) {
+        if (activado == true) {
+            btnInicio.removeEventListener('click', comenzarPartida)
+            btnInicio.style.display = 'none'
+        } else if (activado == false) {
+            btnInicio.addEventListener('click', comenzarPartida)
+            btnInicio.style.display = ''
+        }
+    }  
 }
 
-
-const btnReset = document.getElementById("reset")
-btnReset?.addEventListener('click', reset)
+const prfCogeCarta = document.getElementById("txtCogeCarta")
+function btnCogerCartaActivado(activado: boolean) {
+    if ((btnCogerCarta && btnCogerCarta instanceof HTMLImageElement) && (prfCogeCarta && prfCogeCarta instanceof HTMLParagraphElement)) {
+        if (activado == true) {
+            btnCogerCarta.addEventListener('click', pedirCarta)
+            prfCogeCarta.innerHTML = 'Coge una carta'
+        } else if (activado == false) {
+            btnCogerCarta.removeEventListener('click', pedirCarta)
+            prfCogeCarta.innerHTML = ''
+        }
+    }  
+}
